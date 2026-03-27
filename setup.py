a7d7a82658a9a4f750e88129ed89c5ae88b02542
kikiiuri <c.iurilli@campus.unimib.it>
2026-03-27 15:58:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Lib_Gamma", # Il nome che userai per importarla
+    name="Lib_Gamma",
     version="0.1",
-    packages=find_packages(),
-    install_requires=[], # Qui metterai eventuali altre librerie da cui dipendi
+    packages=["Lib_Gamma"], # Specifichiamo direttamente il nome della cartella
+    install_requires=[],
 )
