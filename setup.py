4097b29b74dee90b8b9dc97f459ba6c8172686ae
kikiiuri <c.iurilli@campus.unimib.it>
2026-03-27 11:36:35 +0100

    Initialize setup.py for Lib_Gamma package

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Lib_Gamma", # Il nome che userai per importarla
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[], # Qui metterai eventuali altre librerie da cui dipendi
+)
